9443406e48a254f7797320bcbc46ae050f3ffde2
MacBook Pro <macbookpro@Tanyos-macbookpro.local>
2022-03-14 15:54:20 +0200

    email validation on login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,9 +1,20 @@
 const { loginUser } = require("../services/userService");
+const { validationResult } = require("express-validator");
 
 const loginController = async (req, res) => {
     const { email, password } = req.body;
+    const errors = validationResult(req);
 
+    
     try {
+
+        // if there are errors from the express-validator
+        if(errors.length) {
+            errors.errors.forEach(error => {
+                throw Error(error.msg);
+            })
+        }
+
         const token = await loginUser(email, password);
         res.json({ token });
     } catch(err) {
